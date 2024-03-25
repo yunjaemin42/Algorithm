d15e606782f10feef5868afa9832379950c71dde
yunjaemin42 <A20213175@HackerLogin.8.8.8.8>
2024-03-25 20:34:13 +0900

    solve 1001

--- backjoon/1001/1001.c
+++ backjoon/1001/1001.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello world");
-}
+    printf("int 자료형의 크기: %d바이트\n", sizeof(int));
+    printf("float 자료형의 크기: %d바이트\n", sizeof(float));
+    printf("char 자료형의 크기: %d바이트\n", sizeof(char));
+    printf("double 자료형의 크기: %d바이트\n",sizeof(double));
+} 
